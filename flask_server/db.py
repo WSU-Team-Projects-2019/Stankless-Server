bc4b6f59d91064a5c8d77379e177639397b3ae77
blastbeatsandcode <alexsilcott@gmail.com>
2019-03-24 18:54:43 -0400

    added logic to check if barcode already exists in database, and if it does, increment the count instead of inserting a new row.

--- flask_server/db.py
+++ flask_server/db.py
@@ -8,17 +8,26 @@ def insert_barcode(title, barcode):
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
 
+    # count to hold the number of items already in database
+    count = 1
+
+    # check to see if the item already exists in the database
+    query = "SELECT item_id, count FROM barcodes WHERE number = {};".format(barcode)
+    cursor.execute(query)
+    # Fetch a single row and check if it is equal to None
+    data = cursor.fetchone()
+    query = "INSERT INTO barcodes (title, number, count) VALUES (\'{}\', \'{}\', {});".format(title, barcode, count)
+    if data != None: # If barcode already exists in the database
+        print("Got to here! Values are ({}, {})".format(data[0], data[1]))
+        item_id = data[0] # Store values from the returned tuple
+        count = data[1] + 1
+        query = "UPDATE barcodes SET count = {} WHERE item_id = {}".format(count, item_id)
+
     # execute SQL query using execute() method.
-    query = "INSERT INTO barcodes (title, number, count) VALUES (%s, %s, 0)"
-    values = (title, barcode)
-    cursor.execute(query, values)
+    cursor.execute(query)
 
     # Commit the changes
     db.commit()
-
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchone()
-    print("MySQL Response : %s " % data)
 
     # disconnect from server
     db.close()
